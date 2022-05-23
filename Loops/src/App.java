b097b344dd5ee3b01321fa8b971ff2263d79ae8f
vinod <vinod.chattergee@tesco.com>
2022-05-23 13:44:44 +0530

    loops

--- Loops/src/App.java
+++ Loops/src/App.java
@@ -1,78 +1,46 @@
-import java.sql.SQLOutput;
-
 public class App {
     public static void main(String[] args) {
-        String car = "Honda BRV";
-        int price = 140000;
-        int moneyInBank = 1000000000;
-        boolean isDamaged = true;
+        // While Loop
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i + ". Hi its from while loop");
+            i++;
+        }
+        System.out.println("value of i is : " + i);
+        // Do While
 
-        //        Arithmetic Operators
-        System.out.println("Price of the car " + car + " is INR " + price);
-        int increasePrice = price + 8000;
-        System.out.println("Price of the car " + car + " is increased to INR " + increasePrice);
-        int decreasedPrice = price - 8000;
-        System.out.println("Price of the car " + car + " is decreased to INR " + decreasedPrice);
-        int twoCarsPrice = price * 2;
-        System.out.println("Price of the 2 cars of model " + car + " is INR " + twoCarsPrice);
-        int brvsYouCanBuy = moneyInBank / price;
-        System.out.println("You can buy " + brvsYouCanBuy + " car(s) of model " + car + " with your money in bank");
-        int moneyRemaining = moneyInBank % price;
-        System.out.println("You will have " + moneyRemaining + " after buying " + brvsYouCanBuy + " BRVs");
-        System.out.println();
-        //        UnaryOperators
-        System.out.println();
-        int priceNegative = -140000;
-        int priceNegativeWithPlusSign = +priceNegative;
-        System.out.println("Negative price with plus sign : INR " + priceNegativeWithPlusSign);
-        int priceNegativeWithMinusSign = -priceNegative;
-        System.out.println("Negative price with minus sign : INR " + priceNegativeWithMinusSign);
-        int oneRupeePriceIncrease = price++;//price has become 140001 but is evaluated after assignment.
-        System.out.println("Price didnt increased by 1 rupee as the evaluation happened after the price is assigned. " + oneRupeePriceIncrease);
-        int oneRupeePriceIncrease2 = ++price;//price has become 140002 and is evaluated before assignment.
-        System.out.println("Price is increased by 2 rupee as the evaluation is now happened before the assignment. " + oneRupeePriceIncrease2);
-        int oneRupeePriceDecrease = price--;//price has become 140001 but is evaluated after assignment.
-        System.out.println("Price didnt decreased by 1 rupee as the evaluation happened after the price is assigned. " + oneRupeePriceDecrease);
-        int oneRupeePriceDecrease2 = --price;//price has become 140000 and is evaluated before assignment.
-        System.out.println("Price is decreased by 2 rupee as the evaluation is now happened before the assignment. " + oneRupeePriceDecrease2);
-        System.out.println("The car is damaged? : " + !isDamaged);
-        System.out.println();
+        int j = 1;
+        do {
+            System.out.println(j + ". Hi its from do while loop");
+            j++;
+        } while (j <= 5);
+        System.out.println("value of j is : " + j);
+        // For loop
+        int k;
+        for (k = 0; k <= 5; k++) {
+            System.out.println(k + ". Hi its from for loop");
+        }
+        System.out.println("value of k is : " + k);
+        // Continue and break
 
-        //        Equality And Relational Operators
-        System.out.println();
-        System.out.println("The cars price is equal to the money in the bank : " + (moneyInBank == price));
-        System.out.println("The cars price is not equal to the money in the bank : " + (moneyInBank != price));
-        System.out.println("The cars price is greater than the money in the bank : " + (moneyInBank < price));
-        System.out.println("The cars price is lesser than the money in the bank : " + (moneyInBank > price));
-        System.out.println("The cars price is greater than or equal to the money in the bank : " + (moneyInBank <= price));
-        System.out.println("The cars price is lesser than or equal to the money in the bank : " + (moneyInBank >= price));
-        System.out.println("The car dataType is String : " + (car instanceof String));
-        System.out.println();
-        //        Ternary Operators
-        String damagedText = isDamaged ? "The car is damaged " : " The car is not damaged";
-        System.out.println("The cars status : " + damagedText);
-        System.out.println();
+        int l;
+        for (l = 0; l <= 5; l++) {
+            if (l == 4) continue;
+            System.out.println(l + ". Hi its from for-loop-continue");
+        }
+        System.out.println("value of l is : " + l);
 
-        //          Logical Operators
-        String worthSeeingTheCarText = !isDamaged || price <= 120000 ? "Its is worth seeing the car." : "It is not worth seeing the car.";
-        System.out.println(" Cars current value : " + worthSeeingTheCarText);
-        String worthRepairingTheCarText = isDamaged && price <= 100000 ? "It is worth buying and repairing the car." : "It is not worth buying and then repairing the car.";
-        System.out.println(" Cars current value : " + worthRepairingTheCarText);
-        //        Assignment Operators
+        int m;
+        for (m = 0; m <= 5; m++) {
+            if (m == 3) break;
+            System.out.println(m + ". Hi its from for-loop-break");
+        }
+        System.out.println("value of l is : " + m);
 
-        price+=1000;
-        System.out.println("The price is increased INR "+price);
-        price-=2000;
-        System.out.println("The price is decreased INR "+price);
-        price*=2;
-        System.out.println("The price is multiplied INR "+price);
-        price/=2;
-        System.out.println("The price is divided INR "+price);
-        price%=10000;
-        System.out.println("The price remained INR "+price);
-
-
-// Test
-
+        for (int n = 0; n < 10; n++) {
+            for (int o = 0; o < 5; o++) {
+                System.out.println(n + "." + o + ". Hi its from nested for loop");
+            }
+        }
     }
 }
